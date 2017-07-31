a8dbd68ffa8cfd3fd3bb41899217736baac397b2
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-07-30 19:00:19 -0600

    Updated

---
+++ server.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?php
+error_reporting(0);
+//added Dec 3 2015
+ob_implicit_flush();
+
+include 'php_serial.class.php';
+include_once('projectorCommands.inc');
+
+$skipJSsettings = 1;
+include_once '/opt/fpp/www/config.php';
+include_once '/opt/fpp/www/common.php';
+
+$pluginName  = "FPPViewerVotingServer";
+
+include_once 'functions.inc.php';
+include_once 'commonFunctions.inc.php';
+
+$logFile = "/tmp". "/".$pluginName.".log";
+
+
+
+$pluginConfigFile = (__DIR__)."/plugin." .$pluginName;
+logEntry("PluginConfig File: ".$pluginConfigFile);
+
+if (file_exists($pluginConfigFile))
+	$pluginSettings = parse_ini_file($pluginConfigFile);
+
+
+$myPid = getmypid();
+
+//$PORT = ReadSettingFromFile("PORT",$pluginName);
+$PORT = $pluginSettings['PORT'];
+
+//$ENABLED = ReadSettingFromFile("ENABLED",$pluginName);
+$ENABLED = urldecode($pluginSettings['ENABLED']);
+?>
