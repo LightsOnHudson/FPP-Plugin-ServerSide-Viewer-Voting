de8d73a745f566b20ef1ee4603a15197a9994958
LightsOnHudson <bshaver@officemini.home.lan>
2017-08-31 10:20:18 -0600

    Update

--- server.php
+++ server.php
@@ -160,7 +160,7 @@ if($SEQUENCE_WITH_HIGHEST_VOTES_FOR_SITE_ID != 0) {
 	//$FSEQ = urldecode($FSEQ);
 	$FSEQ=substr(urldecode($FSEQ), 0, (strlen ($FSEQ))) - (strlen (strrchr(urldecode($FSEQ),'.')));
 	
-	
+	logEntry("Sending fseq: ".$FSEQ." back to system that asked");
 }
 
 
